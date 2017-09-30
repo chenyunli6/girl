e3dc45c994bf0cd99e67139e1f5709c1e050f22d
chenyunli <18301662790@163.com>
2017-09-30 11:29:45 +0800

    record some request info, such as url,ip,params and so on

--- src/main/java/com/yunli/girl/aspect/HttpAspect.java
+++ src/main/java/com/yunli/girl/aspect/HttpAspect.java
@@ -1,12 +1,15 @@
 package com.yunli.girl.aspect;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Aspect
 @Component
@@ -19,12 +22,35 @@ public class HttpAspect {
     }
 
     @Before("log()")
-    public void doBefore() {
+    public void doBefore(JoinPoint joinPoint) {
         logger.info("11111111");
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // url
+        logger.info("url={}", request.getRequestURL());
+
+        // method
+        logger.info("method={}", request.getMethod());
+
+        // ip
+        logger.info("ip={}", request.getRemoteAddr());
+
+        // class method
+        logger.info("class_method={}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+
+        // params
+        logger.info("args={}", joinPoint.getArgs());
     }
 
     @After("log()")
     public void doAfter() {
         logger.info("222222222");
+    }
+
+    @AfterReturning(returning = "object", pointcut = "log()")
+    public void doAfterReturnig(Object object) {
+        logger.info("response={}", object);
     }
 }
